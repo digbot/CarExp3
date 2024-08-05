8780785375610e4f2d0ccc0492f87b724732f1b0
Dimitar Sashev Dimitrov <dimitar.sashev@gmail.com>
2024-08-05 12:30:37 +0300

    add_m_dist

--- src/main/java/com/digbot/CarExp3/data/DistanceProfile.java
+++ src/main/java/com/digbot/CarExp3/data/DistanceProfile.java
@@ -10,10 +10,12 @@ public class DistanceProfile extends SizeProfile {
         switch (distance)  {
             case Distance.LONG:
                 this.fuel = this.fuel * 4;
+                this.maintenance =  this.maintenance * 4;
                 System.out.println("Distance.LONG  %d" +  this.fuel );
                 break;
             case Distance.MIDDLE:
                 this.fuel = this.fuel * 2;
+                this.maintenance = this.maintenance * 2;
                 System.out.println("Distance.MIDDLE  %d" +  this.fuel );
                 break;
             default:
