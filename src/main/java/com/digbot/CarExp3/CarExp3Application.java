3e1f8e54a4c767c4feb1afd269d7dad1727244a1
Dimitar Sashev Dimitrov <dimitar.sashev@gmail.com>
2025-02-10 21:31:04 +0200

    hotix

--- src/main/java/com/digbot/CarExp3/CarExp3Application.java
+++ src/main/java/com/digbot/CarExp3/CarExp3Application.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-st
+public class CarExp3Application {
+
 	public static void main(String[] args) {
 		SpringApplication.run(CarExp3Application.class, args);
 	}
